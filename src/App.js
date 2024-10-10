51f05e709dc275320fc1698b4d2ec141dbc21e00
gustavfaustino <gustav.faustino107@gmail.com>
2024-10-10 12:59:48 -0300

    Ajustes básicos na Sidebar lateral; Ajustes no CSS

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import './App.css';
+import gptLogo from './assets/chatgpt.svg';
+import addBtn from './assets/add-30.png';
+import msgIcon from './assets/message.svg';
+import homeIcon from './assets/home.svg';
+import savedIcon from './assets/bookmark.svg';
+import upgradeIcon from './assets/rocket.svg';
+// import aboutIcon from './assets/about.svg';
 
 function App() {
   return (
     <div className="App">
-      <div className="Sidebar">
+      <div className="sideBar">
         <div className='upperSidebar'>
           <div className='upperSidebarTop'>
-            <img src='https://via.placeholder.com/150' alt='logo do site' className='logo' />
-            <span className='LogoText'>Gepeto</span>
-            <button className='midBtn'>
-              <img src="" alt="" className="addBtn" /> New Project 
-            </button>
-            <div className='upperSideBotton'>
-
-              <button className='queryBtn'><img src="" alt="" className="query" /> Eu sou um menino de verdade?</button>
-              <button className='queryBtn'><img src="" alt="" className="query" /> Qual é a cor do cavalo branco de Napoleão?</button>
-
-            </div>
-
+            <img src={gptLogo} alt='logo do site' className='logo' />
+            <span className='brand'>GePeTo</span></div>
+          <button className='midBtn'><img src={addBtn} alt="New Chat" className="addBtn" />New Chat</button>
+          <div className='Queries'>
+            <button className='query'><img src={msgIcon} alt="Query" /> Query 1</button>
+            <button className='query'><img src={msgIcon} alt="Query" /> Query 2</button>
           </div>
         </div>
-
-        <div className='lowerSidebar'></div>
+        <div className='lowerSidebar'>
+          <div className='listItems'><img src={homeIcon} alt='Home button' className='listItemIMG' />HOME</div>
+          <div className='listItems'><img src={savedIcon} alt='Saved queries icon' className='listItemIMG' />SAVED</div>
+          <div className='listItems'><img src={upgradeIcon} alt='' className='listItemIMG' />UPGRADE</div>
+        </div>
       </div>
-
       <div className="Main">
 
       </div>
     </div >
+
   );
 }
 
